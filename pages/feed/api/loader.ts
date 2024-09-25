62db91c89303e35283940f85395fc31e7d39c2bd
yashiking <jam.tennis57@gmail.com>
2024-09-26 04:55:02 +0900

    get articles with tag query

--- pages/feed/api/loader.ts
+++ pages/feed/api/loader.ts
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import type { FetchResponse } from "openapi-fetch";
 import { promiseHash } from "remix-utils/promise";
 
@@ -16,10 +16,15 @@ async function throwAnyErrors<T, O, Media extends `${string}/${string}`>(
   return data as NonNullable<typeof data>;
 }
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const selectedTag = url.searchParams.get("tag") ?? undefined;
+
   return json(
     await promiseHash({
-      articles: throwAnyErrors(GET("/articles")),
+      articles: throwAnyErrors(
+        GET("/articles", { params: { query: { tag: selectedTag } } }),
+      ),
       tags: throwAnyErrors(GET("/tags")),
     }),
   );
