8cc1d9cef7efecc517bdc82da2a79eda12ad0b3d
yashiking <jam.tennis57@gmail.com>
2024-09-26 04:46:53 +0900

    add ui

---
+++ pages/feed/ui/ArticlePreview.tsx
@@ -0,0 +1,51 @@
+import { Link } from "@remix-run/react";
+import type { Article } from "shared/api";
+
+interface ArticlePreviewProps {
+  article: Article;
+}
+
+export function ArticlePreview({ article }: ArticlePreviewProps) {
+  return (
+    <div className="article-preview">
+      <div className="article-meta">
+        <Link to={`/profile/${article.author.username}`} prefetch="intent">
+          <img src={article.author.image} alt="" />
+        </Link>
+        <div className="info">
+          <Link
+            to={`/profile/${article.author.username}`}
+            className="author"
+            prefetch="intent"
+          >
+            {article.author.username}
+          </Link>
+          <span className="date" suppressHydrationWarning>
+            {new Date(article.createdAt).toLocaleDateString(undefined, {
+              dateStyle: "long",
+            })}
+          </span>
+        </div>
+        <button className="btn btn-outline-primary btn-sm pull-xs-right">
+          <i className="ion-heart"></i> {article.favoritesCount}
+        </button>
+      </div>
+      <Link
+        to={`/article/${article.slug}`}
+        className="preview-link"
+        prefetch="intent"
+      >
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+        <span>Read more...</span>
+        <ul className="tag-list">
+          {article.tagList.map((tag) => (
+            <li key={tag} className="tag-default tag-pill tag-outline">
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </Link>
+    </div>
+  );
+}
---
+++ shared/api/index.ts
@@ -0,0 +1,2 @@
+export { GET, POST, PUT, DELETE } from "./client";
+export type { Article } from "./model";
