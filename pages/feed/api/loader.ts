7fb4ddfb6ec9c6f10fd9ff0031e02db0978dcee7
yashiking <jam.tennis57@gmail.com>
2024-09-26 21:22:21 +0900

    Tabs

--- pages/feed/api/loader.ts
+++ pages/feed/api/loader.ts
@@ -2,7 +2,7 @@ import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import type { FetchResponse } from "openapi-fetch";
 import { promiseHash } from "remix-utils/promise";
 
-import { GET } from "shared/api";
+import { GET, requireUser } from "shared/api";
 
 async function throwAnyErrors<T, O, Media extends `${string}/${string}`>(
   responsePromise: Promise<FetchResponse<T, O, Media>>,
@@ -23,6 +23,27 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const selectedTag = url.searchParams.get("tag") ?? undefined;
   const page = parseInt(url.searchParams.get("page") ?? "", 10);
+
+  if (url.searchParams.get("source") === "my-feed") {
+    const userSession = await requireUser(request);
+
+    return json(
+      await promiseHash({
+        articles: throwAnyErrors(
+          GET("/articles/feed", {
+            params: {
+              query: {
+                limit: LIMIT,
+                offset: !Number.isNaN(page) ? page * LIMIT : undefined,
+              },
+            },
+            headers: { Authorization: `Token ${userSession.token}` },
+          }),
+        ),
+        tags: throwAnyErrors(GET("/tags")),
+      }),
+    );
+  }
 
   return json(
     await promiseHash({
